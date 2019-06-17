9f345d7a517cd17b502ae6bc6e3310593eeebc13
Ifeanyi-Owah <ifeanyi_owah@yahoo.com>
2019-06-16 22:01:29 -0400

    mars-photos-app initial setup

---
+++ src/Header/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import "./Header.css";
+
+class Header extends Component{
+    render(){
+        return(
+            <header>
+                <h1>Mars Photo API</h1>
+                <div className="Header-test">
+
+                </div>
+            </header>
+        );
+    }
+}
+
+
+export default Header;
